40fc2c4e8515ed9b38b65c414c3c621b45970cc3
Fernando Dominguez Pacheco <fernandojavierdominguez@gmail.com>
2020-12-03 12:35:20 -0300

    Mobile Developer Test

--- usecases/src/main/java/com/fernandodominguezpacheco/reign/GetAllStoriesRoom.kt
+++ usecases/src/main/java/com/fernandodominguezpacheco/reign/GetAllStoriesRoom.kt
@@ -5,5 +5,5 @@ import com.fernandodominguezpacheco.reign.repository.StoryRepository
 class GetAllStoriesRoom (private val storyRepository: StoryRepository) {
 
     suspend fun invoke() : List<Story> = storyRepository.getAllStoriesRoom()
-
+    
 }
